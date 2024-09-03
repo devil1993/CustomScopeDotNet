06ec5beb192b0da754135531ec5cc5dcb01e1f0f
Sam Ghatak <samanway.ghatak@hyland.com>
2024-09-03 13:04:08 +0530

    Added scoped service provider.

---
+++ LegacyWebFormApp/DIContainer/ServiceProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LegacyWebFormApp.DIContainer
+{
+    public class ServiceProvider
+    {
+        private static IServiceProvider _provider;
+        private static readonly object _slock = new object();
+
+
+        static ServiceProvider()
+        {
+            if (_provider == null)
+            {
+                lock (_slock)
+                {
+                    if (_provider == null)
+                    {
+                        ServiceCollection coll = GetServiceCollection();
+
+                        _provider = coll.BuildServiceProvider();
+                    }
+                }
+            }
+        }
+
+        private static ServiceCollection GetServiceCollection()
+        {
+            ServiceCollection coll = new ServiceCollection();
+
+            return coll;
+        }
+
+        public static IServiceScope CreateScope()
+        {
+            return _provider.CreateScope();
+        }
+    }
+}
